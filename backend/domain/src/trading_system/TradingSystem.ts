1214fa46d3a4b1cd9973aded958314f609807669
rono <rono@wix.com>
2020-04-04 22:05:53 +0300

    exporting issues

--- backend/domain/src/trading_system/TradingSystem.ts
+++ backend/domain/src/trading_system/TradingSystem.ts
@@ -1,10 +1,14 @@
 import { UserManagement, User } from "../user/internal_api";
-
-class TradingSystem {
+export default class TradingSystem {
   private userManagement: UserManagement;
+  public counter: number;
   constructor() {
     this.userManagement = new UserManagement();
+    this.counter = 0;
+    this.increase = this.increase.bind(this);
+    this.getCounter = this.getCounter.bind(this);
   }
+
   register(userName: string, password: string): void {
     const newUser: User = new User(userName, password);
     const res = this.userManagement.register(newUser);
@@ -12,4 +16,11 @@ class TradingSystem {
   getUserByName(userName: string) {
     return this.userManagement.getUserByName(userName);
   }
+  increase = () => {
+    this.counter++;
+  };
+  getCounter = () => this.counter;
 }
+
+
+
---
+++ backend/service/src/index.ts
@@ -0,0 +1,3 @@
+import  TradingSystem  from "domain_layer";
+const instance = new TradingSystem();
+console.log(instance);
