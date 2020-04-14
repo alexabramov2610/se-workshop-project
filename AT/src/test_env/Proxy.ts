ed72c19710081102ac703117720265f6d792dfbc
rono <rono@wix.com>
2020-04-14 16:43:55 +0300

    added sad path uc 4.3

--- AT/src/test_env/Proxy.ts
+++ AT/src/test_env/Proxy.ts
@@ -1,99 +1,110 @@
-import {Bridge} from './Bridge';
-import {Item, Response, Store, User, Credentials, BuyItem, RATE, SearchData, Cart} from "./types";
+import { Bridge } from "./Bridge";
 import {
-    DummyResponse,
-    DummyItemResponse,
-    DummyStoreResponse,
-    DummyUsersResponse,
-    DummyUserResponse,
-    DummyBuyResponse,
-    DummyPurchaseHistoryResponse,
-    DummySearchResponse, DummyCartResponse
+  Item,
+  Response,
+  Store,
+  User,
+  Credentials,
+  BuyItem,
+  RATE,
+  SearchData,
+  Cart,
+} from "./types";
+import {
+  DummyResponse,
+  DummyItemResponse,
+  DummyStoreResponse,
+  DummyUsersResponse,
+  DummyUserResponse,
+  DummyBuyResponse,
+  DummyPurchaseHistoryResponse,
+  DummySearchResponse,
+  DummyCartResponse,
 } from "../../__tests__/dummy_values/dummyValues";
 
 class Proxy implements Bridge {
-    private real: Bridge;
+  private real: Bridge;
 
-    setReal(adapter: Bridge) {
-        this.real = adapter;
-    }
+  setReal(adapter: Bridge) {
+    this.real = adapter;
+  }
 
-    removeItem(item: Item): Response {
-        return this.real ? this.real.removeItem(item) : DummyResponse;
-    }
+  removeItem(item: Item): Response {
+    return this.real ? this.real.removeItem(item) : DummyResponse;
+  }
 
-    removeStore(store: Store) {
-        return this.real ? this.real.removeStore(store) : DummyResponse;
-    }
+  removeStore(store: Store) {
+    return this.real ? this.real.removeStore(store) : DummyResponse;
+  }
 
-    addStore(store: Store) {
-        return this.real ? this.real.addStore(store) : DummyStoreResponse;
-    }
+  addStore(store: Store) {
+    return this.real ? this.real.addStore(store) : DummyStoreResponse;
+  }
 
-    addItemToStore(store: Store, item: Item) {
-        return this.real ? this.real.addItemToStore(store, item) : DummyResponse;
-    }
+  addItemToStore(store: Store, item: Item) {
+    return this.real ? this.real.addItemToStore(store, item) : DummyResponse;
+  }
 
-    viewItem(item: Item) {
-        return this.real ? this.real.viewItem(item) : DummyItemResponse;
-    }
+  viewItem(item: Item) {
+    return this.real ? this.real.viewItem(item) : DummyItemResponse;
+  }
 
-    viewStore(store: Store) {
-        return this.real ? this.real.viewStore(store) : DummyStoreResponse;
-    }
+  viewStore(store: Store) {
+    return this.real ? this.real.viewStore(store) : DummyStoreResponse;
+  }
 
-    getLoggedInUsers() {
-        return this.real ? this.real.getLoggedInUsers() : DummyUsersResponse;
-    }
+  getLoggedInUsers() {
+    return this.real ? this.real.getLoggedInUsers() : DummyUsersResponse;
+  }
 
-    removeUser(user: User) {
-        return this.real ? this.real.removeUser(user) : DummyResponse;
-    }
+  removeUser(user: User) {
+    return this.real ? this.real.removeUser(user) : DummyResponse;
+  }
 
-    getUserByName(user: User) {
-        return this.real ? this.real.getUserByName(user) : DummyUserResponse;
-    }
+  getUserByName(user: User) {
+    return this.real ? this.real.getUserByName(user) : DummyUserResponse;
+  }
 
-    register(credentials: Credentials) {
-        return this.real ? this.real.register(credentials) : DummyResponse;
-    }
+  register(credentials: Credentials) {
+    return this.real ? this.real.register(credentials) : DummyResponse;
+  }
 
-    login(credentials: Credentials) {
-        return this.real ? this.real.login(credentials) : DummyResponse;
-    }
+  login(credentials: Credentials) {
+    return this.real ? this.real.login(credentials) : DummyResponse;
+  }
 
-    logout() {
-        return this.real ? this.real.logout() : DummyResponse;
-    }
+  logout() {
+    return this.real ? this.real.logout() : DummyResponse;
+  }
 
-    buyItem(transaction: BuyItem) {
-        return this.real ? this.real.buyItem(transaction) : DummyBuyResponse;
-    }
+  buyItem(transaction: BuyItem) {
+    return this.real ? this.real.buyItem(transaction) : DummyBuyResponse;
+  }
 
-    getPurchaseHistory() {
-        return this.real
-            ? this.real.getPurchaseHistory()
-            : DummyPurchaseHistoryResponse;
-    }
+  getPurchaseHistory() {
+    return this.real
+      ? this.real.getPurchaseHistory()
+      : DummyPurchaseHistoryResponse;
+  }
 
-    search(searchData: SearchData): Response {
-        return this.real ? this.real.search(searchData) : DummySearchResponse;
-    }
+  search(searchData: SearchData): Response {
+    return this.real ? this.real.search(searchData) : DummySearchResponse;
+  }
 
-    rate(toRate: Store | Item, rate: RATE): Response {
-        return this.real ? this.real.rate(toRate, rate) : DummySearchResponse;
-    }
+  rate(toRate: Store | Item, rate: RATE): Response {
+    return this.real ? this.real.rate(toRate, rate) : DummySearchResponse;
+  }
 
-    addToCart(item: Item) {
-        return this.real ? this.real.addToCart(item) : DummyResponse;
-    }
+  addToCart(item: Item) {
+    return this.real ? this.real.addToCart(item) : DummyResponse;
+  }
 
-    watchCart() {
-        return this.real ? this.real.watchCart() : DummyCartResponse;
-    }
-    addStoreOwner(user: User){
-        return this.real ? this.real.addStoreOwner() : DummyCartResponse;
-    };
+  watchCart() {
+    return this.real ? this.real.watchCart() : DummyCartResponse;
+  }
+  addStoreOwner(user: User) {
+    return this.real ? this.real.addStoreOwner(user) : DummyCartResponse;
+  }
 }
 
-export {Proxy};
+export { Proxy };
