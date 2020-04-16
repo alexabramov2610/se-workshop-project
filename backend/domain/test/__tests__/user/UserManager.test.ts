b8891acdfd2e936746bd8f8d0f689e9748c402fd
dorbirendorf <dorbiren@gmail.com>
2020-04-16 15:50:31 +0300

    userManager tests added

--- backend/domain/test/__tests__/user/UserManager.test.ts
+++ backend/domain/test/__tests__/user/UserManager.test.ts
@@ -5,6 +5,7 @@ import {Buyer} from "../../../src/user/users/Buyer";
 import {UserManager} from "../../../src/user/UserManager";
 import {RegisteredUser, StoreManager} from "../../../src/user/internal_api";
 import exp from "constants";
+import {errorMsg} from "../../../src/api-int/internal_api";
 
 describe("RegisteredUser Management Unit Tests", () => {
   let driver: UserManagerDriver;
@@ -48,10 +49,25 @@ describe("RegisteredUser Management Unit Tests", () => {
     expect(res.data.result).toBeFalsy();
   });
 
+  test("Login user not exist fail test",()=>{
+    const bob=new Buyer('bob','123456');
+    const req={body:{username:bob.name,password:bob.password},token:bob.UUID}
+    const res:Responses.BoolResponse=this.userManager.login(req);
+    expect(res.data.result).toBeFalsy()
+    expect(res.error.message).toEqual(errorMsg['E_NF']);
+
+  })
+
   test("Login already logged in fail Test", () => {
-    driver.mockWrongPasswordForLoginError();
-    const res: Responses.BoolResponse = driver.loginUser({body:{username:'ron',password:'123456'},token:"token"});
-    expect(res.data.result).toBeFalsy();
+    const bob=new Buyer('bob','123456');
+    const req={body:{username:bob.name,password:bob.password},token:bob.UUID}
+
+    jest.spyOn(this.userManager,'getUserByName').mockReturnValueOnce(bob);
+    jest.spyOn(this.userManager,'getLoggedInUsers').mockReturnValueOnce([bob])
+
+    const res:Responses.BoolResponse=this.userManager.login(req);
+    expect(res.data.result).toBeFalsy()
+    expect(res.error.message).toEqual(errorMsg['E_AL']);
   });
 
 
@@ -251,6 +267,58 @@ describe("RegisteredUser Management Unit Tests", () => {
     expect(userPermissions.length).toBe(0);
 
   });
+
+  test("valid password seccess test", ()=> {
+    const password="123456";
+    const res:boolean=this.userManager.vaildPassword(password);
+    expect(res).toBeTruthy();
+  });
+
+  test("valid password fail test", ()=> {
+    const password="123";
+    const res:boolean=this.userManager.vaildPassword(password);
+    expect(res).toBeFalsy();
+  });
+
+  test("getUserByName seccess test", ()=> {
+    const bob=new Buyer('bob','123456','1');
+    this.userManager.register({body:{username:bob.name,password:bob.password},token:bob.UUID});
+    const res=this.userManager.getUserByName('bob');
+    expect(res).toEqual(bob);
+
+  });
+
+  test("getUserByName fail test", ()=> {
+    const bob=new Buyer('bob','123456','1');
+    this.userManager.register({body:{username:bob.name,password:bob.password},token:bob.UUID});
+    const res=this.userManager.getUserByName('dor');
+    expect(res).toBeFalsy();
+
+  });
+
+  test("getUserByToekn seccess test", ()=> {
+    const bob=new Buyer('bob','123456');
+    this.userManager.register({body:{username:bob.name,password:bob.password},token:bob.UUID});
+    const res=this.userManager.getUserByToken(bob.UUID);
+    expect(res).toEqual(bob);
+
+  });
+
+  test("getUserByToekn fail test", ()=> {
+    const bob=new Buyer('bob','123456');
+    this.userManager.register({body:{username:bob.name,password:bob.password},token:bob.UUID});
+    const res=this.userManager.getUserByToken('5');
+    expect(res).toBeFalsy()
+
+  });
+
+  test('getLoggedInUsers seccess test',()=>{
+    const bob=new Buyer('bob','123456','1');
+    this.userManager.register({body:{username:bob.name,password:bob.password},token:bob.UUID});
+    this.userManager.login({body:{username:bob.name,password:bob.password},token:bob.UUID});
+    const res=this.userManager.getLoggedInUsers();
+    expect(res).toEqual([bob]);
+  })
 
 
 });
