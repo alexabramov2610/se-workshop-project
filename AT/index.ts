6792b668715c7aaa454647be4197296e7f7354cc
rono <rono@wix.com>
2020-04-18 19:09:12 +0300

    added index.ts to main folder

---
+++ AT/index.ts
@@ -0,0 +1,5 @@
+export { Bridge } from "./src/test_env/Bridge";
+export { Proxy } from "./src/test_env/Proxy";
+export { Driver } from "./src/test_env/Driver";
+export * from "./src/test_env/types";
+
