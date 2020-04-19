8646ca81b363ab7b0db8350e929e0bf048467ead
alexabramov2610 <alexabramov2610@gmail.com>
2020-04-19 11:38:37 +0300

    fix verify store operation

--- backend/domain/src/store/Store.ts
+++ backend/domain/src/store/Store.ts
@@ -38,7 +38,6 @@ export class Store {
                 return product;
             }
         }
-        logger.warn(`could not find product with catalog number: ${catalogNumber}`);
         return undefined;
     }
 
