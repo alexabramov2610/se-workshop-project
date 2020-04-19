f4d37b416907af2afd737a85bec4e371e5f270e5
rono <rono@wix.com>
2020-04-19 21:27:01 +0300

    addded store owner tests

--- AT/__tests__/Store_Owner_4/AssignStoreOwner_UC_4.3.test.ts
+++ AT/__tests__/Store_Owner_4/AssignStoreOwner_UC_4.3.test.ts
@@ -1,38 +1,48 @@
-// import { Bridge, Driver } from "../../src/";
-// import { Store, Credentials } from "../../src/test_env/types";
+import { Bridge, Driver } from "../../";
+import { Store, Credentials, User } from "../../src/test_env/types";
 
-// describe("Add Remove Edit Products, UC: 3.2", () => {
-//   let _serviceBridge: Bridge;
-//   let _storeInformation: Store;
-//   let _credentials: Credentials;
+describe("Add Remove Edit Products, UC: 3.2", () => {
+  let _serviceBridge: Bridge;
+  let _storeInformation: Store;
+  let _credentials: Credentials;
+  let _driver: Driver;
 
-//   beforeEach(() => {
-//     _serviceBridge = Driver.makeBridge();
-//     _storeInformation = {
-//       name: "some-mock-store",
-//       description: "selling cool items",
-//       id: "id.stores.boom",
-//     };
-//     _credentials = { userName: "ron", password: "ronpwd" };
-//   });
+  beforeEach(() => {
+    _driver = new Driver()
+      .resetState()
+      .initWithDefaults()
+      .startSession()
+      .registerWithDefaults();
+    _storeInformation = { name: "this-is-the-coolest-store" };
+    _serviceBridge = _driver.getBridge();
+    _storeInformation = { name: "some-store" };
+  });
 
-//   test("Add Store Owner - Happy Path: new user new store", () => {
-    
-//     _serviceBridge.register(_credentials);
-    
-//     const res = _serviceBridge.login(_credentials);
-//     const { asssigned } = _serviceBridge.addStoreOwner(res.data.id).data;
-//     expect(asssigned).toBe(true);
-//   });
+  test("Add Store Owner - Happy Path: new user new store", () => {
+    const newBossCred: Credentials = {
+      userName: "new-boss",
+      password: "owner123",
+    };
+    const newBoss: User = { username: newBossCred.userName };
+    _serviceBridge.register(newBossCred);
 
-  
+    _serviceBridge.login(_driver.getLoginDefaults());
+    const { data } = _serviceBridge.assignStoreOwner(
+      _storeInformation,
+      newBoss
+    );
+    const { storeOwnersNames } = _serviceBridge.viewStore(
+      _storeInformation
+    ).data;
+    expect(storeOwnersNames).toContain(newBoss.username);
+  });
 
-//   test("Add Owner - Sad Path: not logged in user", () => {
-//     _serviceBridge.register(_credentials);
-//     _serviceBridge.logout();
-//     const { error } = _serviceBridge.addStoreOwner({
-//       username: _credentials.userName,
-//     });
-//     expect(error).toBeDefined();
-//   });
-// });
+  // test("Add Owner - Sad Path: not logged in user", () => {
+  //   _serviceBridge.register(_credentials);
+  //   _serviceBridge.logout();
+  //   const { error } = _serviceBridge.addStoreOwner({
+  //     username: _credentials.userName,
+  //   });
+  //   expect(error).toBeDefined();
+  // });
+});
