07986418be105b0970e5ca265f5560b5f2475a60
tal <tallit@post.bgu.ac.il>
2020-04-20 12:26:53 +0300

    add bcrypt

--- backend/domain/src/external_systems/security_system/SecuritySystem.ts
+++ backend/domain/src/external_systems/security_system/SecuritySystem.ts
@@ -1,32 +1,40 @@
-import { BoolResponse,errorMsg } from "../../api-int/internal_api";
+import {BoolResponse, errorMsg} from "../../api-int/internal_api";
+import bcrypt from 'bcrypt'
 
-export class SecuritySystem{
-   private securitySys : any;
-   private name: string;
+export class SecuritySystem {
+    private _securitySys: any;
+    private _name: string;
+    private readonly _saltRounds: number = 10;
 
-   constructor(){
-      this.name = "Security System"
-      this.securitySys=null;
-   }
+    constructor() {
+        this._name = "Security System"
+        this._securitySys = null;
+    }
 
-   setSecuritySys(real: any){
-      this.securitySys=real;
-   }
+    encryptPassword(password: string) {
+        const hash = bcrypt.hashSync(password, this._saltRounds);
+        return hash;
+    }
 
-   connect():BoolResponse{
-      const succ: BoolResponse = { data: {result: true}};
-      if(this.securitySys) {
-         const isConnected = this.securitySys.connect();
-         if(isConnected) 
-         {
-            return succ;
-         }
-         else {
-               return {error: {message: errorMsg['E_CON']+" : " + this.name}, data: {result: this.securitySys.connect()}};}
-      }
-      else{
-         return succ;
-      }
-   }
+    comparePassword(password:string, hash:string){
+       return bcrypt.compareSync(password, hash);
+    }
 
+    /*
+    connect():BoolResponse{
+       const succ: BoolResponse = { data: {result: true}};
+       if(this._securitySys) {
+          const isConnected = this._securitySys.connect();
+          if(isConnected)
+          {
+             return succ;
+          }
+          else {
+                return {error: {message: errorMsg.E_CON+" : " + this._name}, data: {result: this._securitySys.connect()}};}
+       }
+       else{
+          return succ;
+       }
+    }
+ */
 }
