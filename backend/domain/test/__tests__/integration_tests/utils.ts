1f06cd9b1fd917f869d4ae6740027f8b78ff2233
tal <tallit@post.bgu.ac.il>
2020-04-22 11:08:40 +0300

    Guest it

--- backend/domain/test/__tests__/integration_tests/utils.ts
+++ backend/domain/test/__tests__/integration_tests/utils.ts
@@ -1,5 +1,7 @@
 import {TradingSystemManager} from "../../../src/trading_system/TradingSystemManager";
 import * as Req from "../../../src/api-ext/Request";
+import {Product} from "../../../src/trading_system/internal_api";
+import {ProductCategory} from "../../../src/api-ext/Enums";
 
 export default {
     registeredUserLogin: (tradingSystemManager: TradingSystemManager, username: string, password: string): string => {
@@ -14,8 +16,20 @@ export default {
         return token;
     },
 
-    guestLogin: (tradingSystemManager: TradingSystemManager) : string => {
+    guestLogin: (tradingSystemManager: TradingSystemManager): string => {
         return tradingSystemManager.startNewSession();
+    },
+
+    createStore(tradingSystemManager: TradingSystemManager, storeName: string, token:string): void{
+        const req: Req.StoreInfoRequest = {body: {storeName}, token};
+        tradingSystemManager.createStore(req)
+    },
+    addNewProducts(tradingSystemManager: TradingSystemManager, storeName: string, products: Product[], token:string): void{
+        tradingSystemManager.addNewProducts({body:{storeName,products},token});
+    },
+    removeProducts(tradingSystemManager: TradingSystemManager, storeName: string, products: Product[], token:string): void{
+        tradingSystemManager.removeProducts({body:{storeName,products},token});
     }
+
 
 }
