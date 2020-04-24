255708faafd0d42fa949afdb677ef94701a0d69f
rono <rono@wix.com>
2020-04-24 17:06:35 +0300

    added remove manager tests

--- AT/__tests__/Store_Owner_4/RemoveManagers_UC_4.7.test.ts
+++ AT/__tests__/Store_Owner_4/RemoveManagers_UC_4.7.test.ts
@@ -5,86 +5,63 @@ describe("Add Remove Edit Products, UC: 3.2", () => {
   let _serviceBridge: Bridge;
   let _storeInformation: Store;
   let _driver: Driver;
+  let _newManagerCreds: Credentials;
   beforeEach(() => {
     _driver = new Driver()
       .resetState()
+      .startSession()
       .initWithDefaults()
       .startSession()
       .registerWithDefaults()
       .loginWithDefaults();
-
+    _newManagerCreds = {
+      userName: "new-manager",
+      password: "newpwd123",
+    };
     _storeInformation = { name: "this-is-the-coolest-store" };
     _serviceBridge = _driver.getBridge();
     _serviceBridge.createStore(_storeInformation);
     _serviceBridge.logout();
-  });
-
-
-  test("Remove managers - remove store manager options- store owner not logged in", () => {
-    const newManager: Credentials = {
-      userName: "new-manager",
-      password: "newpwd123",
-    };
-    _serviceBridge.register(newManager);
-    _serviceBridge.login(_driver.getLoginDefaults());
-
-    const { data } = _serviceBridge.assignManager(
-      _storeInformation,
-      newManager
-    );
-    expect(data.result).toBe(true);
+    _serviceBridge.register(_newManagerCreds)
+    _driver.loginWithDefaults();
+    console.log(_serviceBridge.assignManager(_storeInformation, _newManagerCreds));
     _serviceBridge.logout();
-    
   });
 
-  test("Give Manager Permissions - valid store, store owner not logged in", () => {
-    const newManager: Credentials = {
-      userName: "new-manager",
-      password: "newpwd123",
-    };
-
-    _serviceBridge.register(newManager);
-
-    const { data, error } = _serviceBridge.assignManager(
-      _storeInformation,
-      newManager
-    );
-    expect(data).toBeUndefined();
-    expect(error).toBeDefined();
+  xtest("Remove managers - remove store manager options- store owner not logged in", () => {
+    const { data, error } = _serviceBridge.removeStoreManager({
+      body: {
+        storeName: _storeInformation.name,
+        usernameToAssign: _newManagerCreds.userName,
+      },
+    });
+    expect(data.result).toBe(false);
+    expect(error.message).toBeDefined();
   });
 
-  test("Give Manager Permissions - logged in, User with ID is already store manager", () => {
-    const newManager: Credentials = {
-      userName: "new-manager",
-      password: "newpwd123",
-    };
 
-    _serviceBridge.register(newManager);
-    _serviceBridge.login(_driver.getLoginDefaults());
-
-    const res = _serviceBridge.assignManager(_storeInformation, newManager);
-    expect(res.data.result).toBe(true);
-    const { data } = _serviceBridge.assignManager(
-      _storeInformation,
-      newManager
-    );
-
-    expect(data).toBeUndefined();
+  xtest("Remove managers - id is not a manager id", () => {
+    _driver.loginWithDefaults();
+    const { data, error } = _serviceBridge.removeStoreManager({
+      body: {
+        storeName: _storeInformation.name,
+        usernameToAssign: 'no-such-user',
+      },
+    });
+    expect(data.result).toBe(false);
+    expect(error.message).toBeDefined();
   });
 
-  test("Give Manager Permissions - logged in, user id is not in the System", () => {
-    const newManager: Credentials = {
-      userName: "new-manager",
-      password: "newpwd123",
-    };
-
-    _serviceBridge.login(_driver.getLoginDefaults());
-
-    const { data,error } = _serviceBridge.assignManager(
-      _storeInformation,
-      newManager
-    );
-    expect(error).toBeDefined()
-    expect(data).toBeUndefined();
+  test("Remove managers - id is not a manager id", () => {
+    _driver.loginWithDefaults();
+    const { data, error } = _serviceBridge.removeStoreManager({
+      body: {
+        storeName: _storeInformation.name,
+        usernameToAssign: _newManagerCreds.userName,
+      },
+    });
+    expect(data.result).toBe(true);
+    expect(error.message).toBeUndefined();
   });
+
 });
