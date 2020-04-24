eb8a08fa25c3498f9aa9090f79a0534f91c4b5bc
rono <rono@wix.com>
2020-04-24 20:35:47 +0300

    removed the prettier diffs

--- backend/domain/src/api-ext/CommonInterface.ts
+++ backend/domain/src/api-ext/CommonInterface.ts
@@ -1,85 +1,87 @@
-import { ProductCategory, Rating } from "./Enums";
+import {ProductCategory, Rating} from "./Enums";
 
-export { ProductCategory };
+export {ProductCategory}
 
 export interface Item extends ProductCatalogNumber {
-  id: number;
+    id: number
 }
 
 export interface ProductCatalogNumber {
-  catalogNumber: number;
+    catalogNumber: number
 }
 
 export interface Product extends ProductCatalogNumber {
-  name: string;
-  price: number;
-  category: ProductCategory;
+    name: string,
+    price: number,
+    category: ProductCategory
 }
 
 export interface Cart {
-  products: CartProduct[];
+    products: CartProduct[]
 }
 
 export interface CartProduct {
-  storeName: string;
-  bagItems: BagItem[];
+    storeName: string,
+    bagItems: BagItem[]
 }
+
+export interface Purchase {
+    storeName: string,
+    userName: string,
+    item: Item,
+    price: number;
+}
+
+export interface BagItem {
+    product: Product,
+    amount: number;
+    finalPrice?: number
+}
+
+export interface CreditCard {
+    holderName: string,
+    number: number,
+    expMonth: number,
+    expYear: number,
+    ccv: number,
+}
+
+export interface ProductWithQuantity extends ProductCatalogNumber {
+    quantity: number
+}
+
+export interface PriceRange {
+    min: number,
+    max: number
+}
+
+export interface SearchFilters {
+    priceRange?: PriceRange,
+    productRating?: Rating,
+    storeRating?: Rating,
+    productCategory?: ProductCategory
+}
+
+export interface SearchQuery {
+    productName?: string,
+    storeName?: string,
+    // tags: Tag[]
+}
+
 export interface IReciept {
   date: Date;
   purchases: Purchase[];
 }
-export interface Purchase {
-  storeName: string;
-  userName: string;
-  item: Item;
-  price: number;
-}
-
-export interface BagItem {
-  product: Product;
-  amount: number;
-  finalPrice?: number;
-}
-
-export interface CreditCard {
-  holderName: string;
-  number: number;
-  expMonth: number;
-  expYear: number;
-  ccv: number;
-}
-
-export interface ProductWithQuantity extends ProductCatalogNumber {
-  quantity: number;
-}
-
-export interface PriceRange {
-  min: number;
-  max: number;
-}
-
-export interface SearchFilters {
-  priceRange?: PriceRange;
-  productRating?: Rating;
-  storeRating?: Rating;
-  productCategory?: ProductCategory;
-}
-
-export interface SearchQuery {
-  productName?: string;
-  storeName?: string;
-  // tags: Tag[]
-}
 
 export interface StoreInfo {
-  storeName: string;
-  storeRating: Rating;
-  storeOwnersNames: string[];
-  storeManagersNames: string[];
-  productsNames: string[];
+    storeName: string,
+    storeRating: Rating,
+    storeOwnersNames: string[],
+    storeManagersNames: string[],
+    productsNames: string[]
 }
 
 export interface ProductInStore {
-  product: Product;
-  storeName: string;
+    product: Product,
+    storeName: string
 }
