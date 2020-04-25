fbb7680920c321535dea3df2f55525b02a75ed78
rono <rono@wix.com>
2020-04-25 15:33:13 +0300

    fixed compliation issues

--- backend/service/src/buying_service/BuyingService.ts
+++ backend/service/src/buying_service/BuyingService.ts
@@ -1,19 +1,25 @@
 import * as Req from "domain_layer/dist/src/api-ext/Request";
 import * as Res from "domain_layer/dist/src/api-ext/Response";
-import {TradingSystemManager as TS} from "domain_layer/dist/src/trading_system/TradingSystemManager";
+import { TradingSystemManager as TS } from "domain_layer/dist/src/trading_system/TradingSystemManager";
 
-export const purchase = (req: Req.PurchaseRequest, ts: TS): Res.PurchaseResponse => {
-    const isCartOnStock: Res.BoolResponse = ts.verifyCart({body: {}, token: req.token})
-    if (!isCartOnStock.data.result) {
-        return isCartOnStock
-    }
-    //calculate price after discount
-    ts.calculateFinalPrices({body: {}, token: req.token})
-    const isPaid :Res.BoolResponse = ts.pay({body: req.body.payment , token: req.token});
-    if(!isPaid.data.result)
-        return isPaid
-    return ts.purchase(req);
-}
-
-
-
+export const purchase = (
+  req: Req.PurchaseRequest,
+  ts: TS
+): Res.PurchaseResponse => {
+  const isCartOnStock: Res.BoolResponse = ts.verifyCart({
+    body: {},
+    token: req.token,
+  });
+  if (!isCartOnStock.data.result) {
+    return isCartOnStock;
+  }
+  //calculate price after discount
+  //TODO i put a price of 1 just to make it run.
+  ts.calculateFinalPrices({ body: {}, token: req.token });
+  const isPaid: Res.BoolResponse = ts.pay({
+    body: { price: 1, payment: req.body.payment },
+    token: req.token,
+  });
+  if (!isPaid.data.result) return isPaid;
+  return ts.purchase(req);
+};
