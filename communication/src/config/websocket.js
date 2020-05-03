a5e9d9384f21d7151c4eee7ce6abd940f5dbf601
alexabramov2610 <alexabramov2610@gmail.com>
2020-05-03 11:03:50 +0300

    added websocket:  guest & logged in users maps  added functions for broadcasting

--- communication/src/config/websocket.js
+++ communication/src/config/websocket.js
@@ -1,8 +1,10 @@
 const WebSocket = require('ws'); // new
 const url = require('url');
 
-const CLIENTS = new Map;
-let i = 1;
+const LOGGED_IN_CLIENTS = new Map;
+const GUESTS = new Map;
+const GUEST_NAME = 'GUEST';
+let i = 0;
 
 export default (port) => {
 
@@ -10,43 +12,61 @@ export default (port) => {
     const socketServer = new WebSocket.Server({port: port});
 
     socketServer.on('connection', (socketClient, req) => {
-        console.log('connected');
-        // console.log(req.url);
-        console.log(url.parse(req.url, true).query.name); // usage: ws://localhost:3000/?name=ryan
-        CLIENTS.set('test' + i, socketClient);
-        i++;
-        socketClient.send('hola');
+        const username = url.parse(req.url, true).query.name;
+        const guest = !username ? `${GUEST_NAME}_${i++}` : undefined;
+        if (username) {
+            console.log(`${username} connected`);
+            LOGGED_IN_CLIENTS.set(username, socketClient)
+            socketClient.send(`hola ${username}`);
+        }
+        else {
+            console.log(`guest connected: ${guest}`);
+            GUESTS.set(guest, socketClient)
+            socketClient.send(`hola ${guest}`);
+        }
 
         socketClient.on(('message'), (data) => {
-            console.log('closed');
-            console.log('Number of clients: ', socketServer.clients.size);
-            socketClient.send('1111111111hola');
-            sendAll('hey mathererer')
+            socketClient.send(`message back to ${username? username: guest}`);
+            sendAll(`broadcast from ${username?username: guest} motherfuckers, message: ${data}`)
         });
 
-        socketClient.on('close', (data) => {
-            console.log('closed');
-            console.log('Number of clients: ', socketServer.clients.size);
+        socketClient.on('close', (asd ,data) => {
+            if (username) {
+                console.log(username + ' byebyebeye ' + i)
+                LOGGED_IN_CLIENTS.delete(username);
+            }
+            else {
+                console.log(guest + ' byebyebeye ' + i)
+                GUESTS.delete(guest);
+            }
         });
     });
 }
 
 export function sendAll(message) {
-    for (let i=0; i<CLIENTS.length; i++) {
-        CLIENTS.values()[i].send("Broadcast: " + message);
-    }
+    sendToAllLoggedInUsers(message);
+    sendToAllGuests(message);
+}
+
+export function sendToAllGuests(message) {
+    for (let client of GUESTS.values())
+        client.send(message)
+}
+
+export function sendToAllLoggedInUsers(message) {
+    for (let client of LOGGED_IN_CLIENTS.values())
+        client.send(message);
 }
 
 export function sendToGroup(usersGroup, message) {
-    for (let i=0; i<usersGroup.length; i++) {
-        usersGroup[i].send("Broadcast: " + message);
-    }
+    for (let client of usersGroup)
+        client.send(message)
 }
 
 export function isConnected(username) {
-    return CLIENTS.keys().include(username);
+    return LOGGED_IN_CLIENTS.keys().include(username);
 }
 
 export function getSocketByUsername(username) {
-    return CLIENTS.get(username);
+    return LOGGED_IN_CLIENTS.get(username);
 }
