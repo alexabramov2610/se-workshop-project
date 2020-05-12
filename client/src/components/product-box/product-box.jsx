7dda87d999f078e3ef683ca9b59791f5039e7266
rono <rono@wix.com>
2020-05-12 22:29:30 +0300

    added procut grid to store page

---
+++ client/src/components/product-box/product-box.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { AiFillStar } from "react-icons/ai";
+import { FiBox } from "react-icons/fi";
+import { history } from "../../utils/config";
+import * as api from "../../utils/api";
+
+
+export const ProductBox = (props) => (
+  <Card className="text-center grid-item">
+    <Card.Body>
+      <Card.Title>
+        <FiBox style={{ marginRight: "4px", marginBottom: "2px" }} />
+        {props.name}
+      </Card.Title>
+      <Card.Text>
+        <div>Price: {props.price}</div>
+        <div>
+          Rating:{" "}
+          {[1, 2, 3, 4, 5].map(
+            (e, index) =>
+              index < props.rating && (
+                <AiFillStar style={{ marginBottom: "2px" }} />
+              )
+          )}
+        </div>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+);
