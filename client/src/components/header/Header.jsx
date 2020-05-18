189c3c3edda52946bb6f3652cabdad89ebe33bd8
avishaiyaniv <avishaiy@post.bgu.il>
2020-05-18 22:18:43 +0300

    basic store managment page

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -43,7 +43,7 @@ export class Header extends React.Component {
                 style={{ textDecoration: "none", color: "black" }}
                 to="/search"
               >
-                Search
+                SEARCH
               </Link>
             </OptionLink>
 
--- client/src/components/store-menu/store-menu.component.jsx
+++ client/src/components/store-menu/store-menu.component.jsx
@@ -15,24 +15,24 @@ const StoreMenu = ({onChange}) => {
     return (
         <Menu
             onClick={(e) => onChange(e)}
-            defaultSelectedKeys={['1']}
             mode="vertical"
             style={{height: '100%', borderLeft: '1px solid'}}
         >
-            <Menu.Item key="1" icon={<AppstoreAddOutlined />}>View Products</Menu.Item>
+            <Menu.Item key="1" icon={<AppstoreAddOutlined />}>Manage Products</Menu.Item>
             <Menu.Item key="2" icon={<PercentageOutlined />}>Manage Discount Policy</Menu.Item>
             <Menu.Item key="3" icon={<ShoppingOutlined />}>Manage Buying Policy</Menu.Item>
             <Menu.Item key="4" icon={<LockOutlined />}>Manage Permissions</Menu.Item>
 
-            {/*If you need sub menu, this is an example*/}
-            {/*<SubMenu key="sub3" icon={<NotificationOutlined/>} title="subnav 3">*/}
-            {/*    <Menu.Item key="9">option9</Menu.Item>*/}
-            {/*    <Menu.Item key="10">option10</Menu.Item>*/}
-            {/*    <Menu.Item key="11">option11</Menu.Item>*/}
-            {/*    <Menu.Item key="12">option12</Menu.Item>*/}
-            {/*</SubMenu>*/}
         </Menu>
     );
 }
 
 export {StoreMenu};
+
+// If you need sub menu, this is an example
+// <SubMenu key="sub3" icon={<NotificationOutlined/>} title="subnav 3">
+//     <Menu.Item key="9">option9</Menu.Item>
+// <Menu.Item key="10">option10</Menu.Item>
+// <Menu.Item key="11">option11</Menu.Item>
+// <Menu.Item key="12">option12</Menu.Item>
+// </SubMenu>
