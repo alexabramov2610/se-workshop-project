1036935be592487b9f3c06b32181cfca06442343
avishaiyaniv <avishaiy@post.bgu.il>
2020-05-19 11:11:13 +0300

    fixing routing issue from store page

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -16,7 +16,10 @@ const instance = axios.create({
 
 async function init(cb) {
     return Promise.all([
-        instance.get(`${baseDomain}/system/newtoken`), instance.get(`${baseDomain}/system/status`), instance.get(`${baseDomain}/system/healthcheck`)]).then(values => cb({ token: values[0].data, status: values[1].data, isSystemUp: values[2].data.data.result }))
+        instance.get(`${baseDomain}/system/newtoken`),
+        instance.get(`${baseDomain}/system/status`),
+        instance.get(`${baseDomain}/system/healthcheck`)
+    ]).then(values => cb({ token: values[0].data, status: values[1].data, isSystemUp: values[2].data.data.result }))
 }
 
 async function adminInit(firstAdminName, firstAdminPassword) {
