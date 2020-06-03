fca3a93a05ef5a256fa16faaa1cc2e034b1acfd1
alexabramov2610 <alexabramov2610@gmail.com>
2020-06-03 17:13:21 +0300

    store mapper

--- backend/domain/src/store/StoreMapper.ts
+++ backend/domain/src/store/StoreMapper.ts
@@ -1,2 +1,8 @@
+import { IItem, IProduct } from "se-workshop-20-interfaces/dist/src/CommonInterface";
 
+export function productsMapper(products: any) :Map<IProduct, IItem[]>{
+    const productsMapper = new Map<IProduct, IItem[]>();
 
+    
+
+}
