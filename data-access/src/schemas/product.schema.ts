50155d06c087f5a489f49810d519a99d40cf7a08
tal <tallit@post.bgu.ac.il>
2020-06-04 20:13:32 +0300

    defalut values for item

--- data-access/src/schemas/product.schema.ts
+++ data-access/src/schemas/product.schema.ts
@@ -1,17 +1,17 @@
-import { Schema, Types } from "mongoose";
+import {Schema, Types} from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
 
 const productSchema = new Schema({
-  items: [{ type: Types.ObjectId, ref: "items" }],
-  catalogNumber: { type: Number, required: true },
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  category: {
-    type: String,
-    enum: ["GENERAL", "ELECTRONICS", "HOBBIES", "HOME", "CLOTHING"],
-  },
-  rating: { type: Number, enum: [1, 2, 3, 4, 5] },
-},{autoCreate: true});
+    items: {type: [{type: Types.ObjectId, ref: "items"}], default: []},
+    catalogNumber: {type: Number, required: true},
+    name: {type: String, required: true},
+    price: {type: Number, required: true},
+    category: {
+        type: String,
+        enum: ["GENERAL", "ELECTRONICS", "HOBBIES", "HOME", "CLOTHING"],
+    },
+    rating: {type: Number, enum: [1, 2, 3, 4, 5]},
+}, {autoCreate: true});
 
 productSchema.plugin(uniqueValidator);
 export default productSchema;
