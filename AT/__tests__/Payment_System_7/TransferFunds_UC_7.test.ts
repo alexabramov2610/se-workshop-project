61f760f84f7387bcfa7f9c5ce57b295ba27c8568
dorbiren <dorbiren@post.bgu.ac.il>
2020-06-09 22:28:58 +0300

    UC-7 transfer funds AT fix

--- AT/__tests__/Payment_System_7/TransferFunds_UC_7.test.ts
+++ AT/__tests__/Payment_System_7/TransferFunds_UC_7.test.ts
@@ -1,55 +1,58 @@
-// import {
-//     Bridge,
-//     Driver,
-// } from "../../";
-// import {PayRequest} from "se-workshop-20-interfaces/dist/src/Request";
-// import * as utils from "../../utils"
+import {
+    Bridge,
+    Driver,
+} from "../../";
+import {PayRequest} from "se-workshop-20-interfaces/dist/src/Request";
+import * as utils from "../../utils"
 
 
-// describe("Guest buy items, UC: 2.8", () => {
-//     let _driver = new Driver();
-//     let _serviceBridge: Bridge;
-//     let _testPaymentInfo: PayRequest;
+describe("Guest buy items, UC: 2.8", () => {
+    let _driver = new Driver();
+    let _serviceBridge: Partial<Bridge>;
+    let _testPaymentInfo: PayRequest;
 
-//     beforeEach(() => {
-//         _serviceBridge = _driver
-//             .startSession()
-//             .initWithDefaults()
-//             .registerWithDefaults()
-//             .loginWithDefaults()
-//             .getBridge();
+    beforeEach(async() => {
+        let _driver = new Driver();
+        _driver.dropDBDor();
+        await _driver.startSession()
+        await _driver.initWithDefaults()
+        await _driver.registerWithDefaults()
+        await _driver.loginWithDefaults()
+        _serviceBridge = await _driver.getBridge();
 
-//         _testPaymentInfo = {
-//             token: "123", body: {
-//                 payment: _driver.getPaymentInfo().payment,
-//                 price: 100
-//             }
-//         }
+        _testPaymentInfo = {
+            token: "123", body: {
+                payment: _driver.getPaymentInfo().payment,
+                price: 100
+            }
+        }
 
-//     });
+    });
 
 
-//     afterAll(() => {
-//         utils.terminateSocket();
-//     });
+    afterAll(() => {
+        utils.terminateSocket();
+        _driver.dropDBDor();
 
-//     test("Valid request", () => {
-//         const {data, error} = _serviceBridge.pay(_testPaymentInfo);
-//         expect(data).toBeDefined();
-//         expect(error).toBeUndefined();
-//     });
+    });
 
-//     test("Invalid request - missing credit card details", () => {
-//         _testPaymentInfo.body.payment.cardDetails.number = "";
-//         const {data, error} = _serviceBridge.pay(_testPaymentInfo);
-//         expect(error).toBeDefined();
-//         expect(data).toBeUndefined();
-//     });
+    test("Valid request",async () => {
+        const {data, error} =await _serviceBridge.pay(_testPaymentInfo);
+        expect(data).toBeDefined();
+        expect(error).toBeUndefined();
+    });
 
-//     test("Invalid request - missing location details", () => {
-//         _testPaymentInfo.body.payment.address = "";
-//         const {data, error} = _serviceBridge.pay(_testPaymentInfo);
-//         expect(error).toBeDefined();
-//         expect(data).toBeUndefined();
-//     });
-// });
+    test("Invalid request - missing credit card details",async () => {
+        _testPaymentInfo.body.payment.cardDetails.number = "";
+        const {data, error} = await _serviceBridge.pay(_testPaymentInfo);
+        expect(error).toBeDefined();
+        expect(data).toBeUndefined();
+    });
+
+    // test("Invalid request - missing location details", async () => {
+    //     _testPaymentInfo.body.payment.address = undefined;
+    //     const {data, error} =await _serviceBridge.pay(_testPaymentInfo);
+    //     expect(error).toBeDefined();
+    //     expect(data).toBeUndefined();
+    // });
+ });
